e6b79cd70121d3d67deb90bdab16bdcccb4bade9
fayis <fayispvchelari@gmail.com>
2024-02-09 20:21:23 +0530

    Todo model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    email = models.EmailField()
+    start = models.DateTimeField(auto_now=False, auto_now_add=False, null = True, blank = True)
+    end = models.DateTimeField(auto_now=False, auto_now_add=False, null = True, blank = True)
+    location = models.CharField(max_length=500)
+    notes = models.TextField()
+    inform_before = models.BooleanField()
+
