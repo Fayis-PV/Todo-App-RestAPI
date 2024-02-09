ef6e8589edc5853539526599fca5929483cf72a8
fayis <fayispvchelari@gmail.com>
2024-02-09 21:26:52 +0530

    first migrate

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView,ListAPIView,CreateAPIView,RetrieveUpdateDestroyAPIView
 from .models import Todo
 from .serializers import TodoSerializer
+from rest_framework import status
 
 # Create your views here.
 # Home Page
@@ -10,4 +11,6 @@ class IndexListView(ListAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     def get(self,request):
-        return Response('HelloWorld')       
+        data = self.queryset
+        print(data)
+        return Response(data=data,status=status.HTTP_200_OK)   
