6ddf71a925b1a1b9cdf798d87b18db5a009fa646
fayis <fayispvchelari@gmail.com>
2024-02-09 20:22:04 +0530

    model import

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.generics import ListCreateAPIView,ListAPIView,CreateAPIView,RetrieveUpdateDestroyAPIView
+from .models import Todo
 
 
 # Create your views here.
