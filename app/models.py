de534dce14008fd98e46ee0aa879fa994529f147
fayis <fayispvchelari@gmail.com>
2024-02-10 21:38:43 +0530

    custom User

--- app/models.py
+++ app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 # Create your models here.
 class Todo(models.Model):
