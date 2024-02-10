a457c2b2d7f9d7f2b2cae062abebad6621678047
fayis <fayispvchelari@gmail.com>
2024-02-10 07:12:05 +0530

    set inform choice

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class Todo(models.Model):
     email = models.EmailField()
     start = models.DateTimeField(auto_now=False, auto_now_add=False, null = True, blank = True)
     end = models.DateTimeField(auto_now=False, auto_now_add=False, null = True, blank = True)
-    location = models.CharField(max_length=500)
-    notes = models.TextField()
-    inform_before = models.BooleanField()
+    location = models.CharField(max_length=500, null = True, blank = True)
+    notes = models.TextField(null = True, blank = True)
+    inform_before = models.IntegerField(default = 10)
 
--- app/serializers.py
+++ app/serializers.py
@@ -2,8 +2,19 @@ from rest_framework import serializers
 from .models import Todo
 
 # Create Here serializers
+min_choices = (
+    (1 , '1 Minute before'),
+    (2 , '2 Minutes before'),
+    (3 , '3 Minutes before'),
+    (5 , '5 Minutes before'),
+    (10, '10 Minutes before'),
+    (15, '15 Minutes before'),
+    (30, '30 Minutes before'),
+    (60, '1 Hour before')
+)
 
 class TodoSerializer(serializers.ModelSerializer):
+    inform_before = serializers.ChoiceField(min_choices)
     class Meta:
         model = Todo
         fields ='__all__'
